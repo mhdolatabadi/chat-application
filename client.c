bd9cc3b9911a8198b66130239b94548dedee2858
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2021-07-31 13:08:12 +0430

    feat: Add menus
    - Add main menu
    - Add register menu
    - Add login menu

--- client.c
+++ client.c
@@ -1,76 +1,114 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h> 
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#define MAX 80 
+#include <cJSON.h>
+#include <cJSON.c>
+#define MAX 80
 #define PORT 12345
-#define SA struct sockaddr 
+#define SA struct sockaddr
 
-// Function designed for chat between client and server. 
-void chat(int server_socket) 
-{ 
-	char buffer[MAX]; 
-	int n; 
-	while (true) { 
-		bzero(buffer, sizeof(buffer)); 
-		printf("Enter your message: "); 
-		n = 0;
+void fLogin(char username[MAX], char password[MAX])
+{
+}
 
-		// Copy client message to the buffer
-		while ((buffer[n++] = getchar()) != '\n') 
-			;
+void login_menu()
+{
+	int n;
+	char username[MAX];
+	char password[MAX];
 
-		// Send the buffer to server
-		send(server_socket, buffer, sizeof(buffer), 0);
-		bzero(buffer, sizeof(buffer));
+	n = 0;
+	printf("Enter Username:");
+	while ((username[n++] = getchar()) != '\n')
+		;
 
-		// Read the message from server and copy it to buffer
-		recv(server_socket, buffer, sizeof(buffer), 0);
+	n = 0;
+	printf("Enter Password:");
+	while ((password[n++] = getchar()) != '\n')
+		;
 
-		// Print buffer which contains the server message
-		printf("From server: %s", buffer);
+	return fLogin(username, password);
+}
 
-		// If the message starts with "exit" then client exits and chat ends
-		if ((strncmp(buffer, "exit", 4)) == 0) { 
-			printf("Client stopping...\n"); 
-			return; 
-		} 
-	} 
-} 
+void fRegister(char username[MAX], char password[MAX])
+{
+}
 
-int main() 
-{ 
-	int client_socket, server_socket; 
-	struct sockaddr_in servaddr, cli; 
+void register_menu()
+{
+	int n;
+	char username[MAX];
+	char password[MAX];
+
+	n = 0;
+	printf("Enter Username:");
+	while ((username[n++] = getchar()) != '\n')
+		;
+
+	n = 0;
+	printf("Enter Password:");
+	while ((password[n++] = getchar()) != '\n')
+		;
+
+	return fRegister(username, password);
+}
+
+void main_menu(int client_socket)
+{
+	printf("Account menu:\n1: Register\n2: Login");
+	int input = 0;
+	scanf("%d", &input);
+	switch (input)
+	{
+	case 1:
+		register_menu();
+		break;
+	case 2:
+		login_menu();
+		break;
+	default:
+		printf("You've entered invalid number!");
+		break;
+	}
+	return main_menu(client_socket);
+}
+
+int main()
+{
+	int client_socket, server_socket;
+	struct sockaddr_in servaddr, cli;
 
 	// Create and verify socket
-	client_socket = socket(AF_INET, SOCK_STREAM, 0); 
-	if (client_socket == -1) { 
-		printf("Socket creation failed...\n"); 
-		exit(0); 
-	} 
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == -1)
+	{
+		printf("Socket creation failed...\n");
+		exit(0);
+	}
 	else
-		printf("Socket successfully created..\n"); 
+		printf("Socket successfully created..\n");
 
 	// Assign IP and port
-	bzero(&servaddr, sizeof(servaddr)); 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-	servaddr.sin_port = htons(PORT); 
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(PORT);
 
-	// Connect the client socket to server socket 
-	if (connect(client_socket, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-		printf("Connection to the server failed...\n"); 
-		exit(0); 
-	} 
+	// Connect the client socket to server socket
+	if (connect(client_socket, (SA *)&servaddr, sizeof(servaddr)) != 0)
+	{
+		printf("Connection to the server failed...\n");
+		exit(0);
+	}
 	else
-		printf("Successfully connected to the server..\n"); 
+		printf("Successfully connected to the server..\n");
 
-	// Function for chat 
-	chat(client_socket); 
+	// Function for chat
+	main_menu(client_socket);
 
-	// Close the socket 
-	shutdown(client_socket, SHUT_RDWR); 
-} 
+	// Close the socket
+	shutdown(client_socket, SHUT_RDWR);
+}
